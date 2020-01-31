694f556e8975c3c1f3e59aaded3c6bd6b100cb24
s20612 <s20612@pjwstk.edu.pl>
2020-01-31 09:44:59 +0100

    Added FileInputStream with StringBuilder

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,29 @@
 package main;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Present your part");
+
+        FileInputStream fis = null;
+
+        try{
+            fis = new FileInputStream("hamster.txt");
+
+            StringBuilder sb = new StringBuilder();
+            int c;
+
+            while((c=fis.read()) != -1){
+                sb.append((char)c);
+            }
+            System.out.println(sb);
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
